3fc8aeba6fe530b0a0764228b41c6b037845b469
Joshua759611 <musyokijoxhua@gmail.com>
2022-05-25 00:12:58 -0400

    updates

--- router/router.js
+++ router/router.js
@@ -1,11 +1,16 @@
 const { request, response } = require('express')
+
+//import the schema from signupmodels.js
 const signUpTemplateCopy=require('../models/signupmodels')
 const express =require('express')
-const router=express.router()
+const router=express.Router()
 router.post('/signup"',(request,response)=>{
     //response.send('send')
+
+    /*creating a variable signedUpUsers which is an instance of signUpTemplateCopy such that when the
+    user signs up on signup page it is going to create an instance of the signedUpUsers*/
     const signedUpUsers=new signUpTemplateCopy({
-        fullName:request.body.fullName,
+        fullName:request.body.fullName,//this means grab the fullName from the body of the request the user has made
         userName:request.body.userName,
         email:request.body.email,
         password:request.body.password
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express =require('express')
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
+const routesUrls=require('./router/router.js')
+const cors= require('cors')
 dotenv.config()
 mongoose.connect(process.env.DATABASA_ACCESS,()=>console.log('Database Connected Successfully'))
 const app= express()
+app.use(express.json())//activating body-parser
+app.use(cors)
+app.use('/app',routesUrls)
+
 app.listen(4000,()=>console.log('Server is up and running'))
